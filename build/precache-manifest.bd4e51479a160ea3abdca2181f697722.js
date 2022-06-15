59c26534e1a3698d5d270f63eb80d62073a37d52
Gabriela Papin <gabrielapapin20@gmail.com>
2022-06-15 19:04:43 -0300

    removendo a pasta build do gitignore

---
+++ build/precache-manifest.bd4e51479a160ea3abdca2181f697722.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73cf9e418990c043e37ca376d588f5fa",
+    "url": "/project-trybewallet/index.html"
+  },
+  {
+    "revision": "014c1b23f4fe7d1670f0",
+    "url": "/project-trybewallet/static/css/main.9f535122.chunk.css"
+  },
+  {
+    "revision": "dfdea476653a39f0a857",
+    "url": "/project-trybewallet/static/js/2.e82e631c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project-trybewallet/static/js/2.e82e631c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "014c1b23f4fe7d1670f0",
+    "url": "/project-trybewallet/static/js/main.2da6f9e0.chunk.js"
+  },
+  {
+    "revision": "f7e9fac6e1746606b827",
+    "url": "/project-trybewallet/static/js/runtime-main.29301b93.js"
+  }
+]);
